094bbae7372defecf1eb6362836dbcc10d93b405
kennLulzsec <159522056+kennLulzsec@users.noreply.github.com>
2024-04-12 02:45:12 +0700

    Update mustjoin.py

--- Musikbot/plugins/bot/mustjoin.py
+++ Musikbot/plugins/bot/mustjoin.py
@@ -6,7 +6,7 @@ from Musikbot import app
 
 #--------------------------
 
-MUST_JOIN = "MusicStreamSupport"
+MUST_JOIN = "KennXhh"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):

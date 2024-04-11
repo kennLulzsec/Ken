a75996669e25c5dee7f432374b8af30f581879a2
kennLulzsec <159522056+kennLulzsec@users.noreply.github.com>
2024-04-12 01:57:18 +0700

    Update config.py

--- config.py
+++ config.py
@@ -54,7 +54,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Vooyage21/ty.git",
+    "https://github.com/kennLulzsec/Ken.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
